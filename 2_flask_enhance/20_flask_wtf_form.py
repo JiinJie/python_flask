44120da342bd6739cd8444b8befacbb6b9bfd917
jiinjie <1280414139@qq.com>
2023-01-09 20:17:38 +0800

    add ohter files

---
+++ 2_flask_enhance/20_flask_wtf_form.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/23 10:29
+# @Author  : jinjie
+
+# 在与服务器交互前，在前端对数据进行检查 成为 表单验证
+#  安装  pip install flask-wtf
+#  安装  pip install email-validator   (需要单独安装)
+
+# 参考D_wtf_form
+
